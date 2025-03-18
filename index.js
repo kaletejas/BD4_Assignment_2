dd6c6d6d6bf2685bcc66fce02f0c884293b3ac96
kaletejas <tejasvijaykale@gmail.com>
2025-03-18 14:49:41 -0400

    added endpoint 1 : get all games

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { resolve } = require('path');
 const cors = require('cors');
 const { open } = require('sqlite');
+const { resolveSoa } = require('dns');
 const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
@@ -16,6 +17,27 @@ let db;
     driver: sqlite3.Database,
   });
 })();
+
+async function fetchAllGames(){
+  let query = 'SELECT * FROM games'
+  let response = await db.all(query, [])
+  return {games : response}
+}
+
+//Exercise 1: Get All Games
+app.get('/games', async (req,res)=>{
+  try{
+    let results = await fetchAllGames();
+    if(results.games.length === 0 ){
+      res.status(404).json({message : 'Games not found'})
+    }
+    res.status(200).json(results)
+  }catch(error){
+    res.status(500).json({error : error.message})
+  }
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
